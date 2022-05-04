890f05acbf63621eb7cb3103dbd3c9b5fbdadeec
cocodevweb <83016254+cocodevweb@users.noreply.github.com>
2022-05-04 13:22:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
  const fs = require("fs"); //fs <-- FileSystem
+ const { multiply, divide } = require("./functions");
+ const { chain } = require("mathjs");
 
  // readFileSync <-- lire le contenu d'un fichier
  // WriteFileSync <-- écrire un fichier avec du contenu
@@ -8,3 +10,12 @@
 
  const fileContent = fs.readFileSync("test.txt", "utf-8");
  console.log(fileContent);
+
+ console.log(multiply(5, 10));
+ console.log(divide(10, 5));
+ 
+ console.log(chain(3)
+ .add(4)
+ .multiply(2)
+ .divide(5)
+ .done())
